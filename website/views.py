7eec76c73286d2e3267311f7a169bccf3d7e3378
Joe Foster <joe.foster@protonmail.com>
2024-08-13 11:57:10 -0700

    docker containerisation and lightsail deployment

--- website/views.py
+++ website/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect, url_for
 
 views = Blueprint("views", __name__)
 
@@ -16,7 +16,7 @@ def joebird():
 
 @views.route("/gallery")
 def gallery():
-    return render_template("gallery1.html")
+    return render_template("gallery.html")
 
 
 @views.route("/projects")
@@ -24,6 +24,22 @@ def projects():
     return render_template("projects.html")
 
 
+@views.route("/login", methods=["POST", "GET"])
+def login():
+
+    if request.method == "POST":
+        user = request.form["nm"]
+        return redirect(url_for("views.user", usr=user))
+    else:
+        return render_template("login.html")
+
+
+@views.route("/<usr>")
+def user(usr):
+    return f"user is {usr}."
+
+
 @views.route("/<name>")
 def say_hello(name):
-    return f'Hello {name}, welcome to my website! <a href="/">Go home</a>'
+    # return f'Hello {name}, welcome to my website! <a href="/">Go home</a>'
+    return render_template("name.html", name=name)
