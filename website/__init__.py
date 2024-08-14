4f7e326ffdf650eb892bc74d13e1af2cdcda687b
Joe Foster <josephrfoster@protonmail.com>
2024-08-14 15:39:59 -0700

    Adding a database model for users

--- website/__init__.py
+++ website/__init__.py
@@ -1,19 +1,12 @@
-from os import path
-
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-
-
-# initialise database object
-db = SQLAlchemy()
-DB_NAME = "database.db"
 
 
 def create_app() -> Flask:
     app = Flask(__name__)
     app.config["SECRET_KEY"] = "1234"
-    app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{DB_NAME}"
-    db.init_app(app)
+    app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///users.sqlite3"
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
     # import blueprint so that app knows available routes
     from .views import views
@@ -22,12 +15,26 @@ def create_app() -> Flask:
     app.register_blueprint(auth, url_prefix="/")
     app.register_blueprint(views, url_prefix="/")
 
-    create_database(app)
+    db = SQLAlchemy(app)
+
+    # Table in database
+    class Users(db.Model):
+        _id = db.Column("id", db.Integer, primary_key=True)  # column 1
+        name = db.Column("name", db.String(100))
+        email = db.Column("email", db.String(100))
+
+        def __init__(self, name, email) -> None:
+            self.name = name
+            self.email = email
+
+    db.create_all()
 
     return app
 
 
-def create_database(app: Flask) -> None:
-    if not path.exists("website/" + DB_NAME):
-        db.create_all(app=app)
-        print("Created Database!")
+# def create_database(app: Flask) -> None:
+#     db = SQLAlchemy()
+#     DB_NAME = "database.db"
+#     if not path.exists("website/" + DB_NAME):
+#         db.create_all(app=app)
+#         print("Created Database!")
--- website/views.py
+++ website/views.py
@@ -40,11 +40,19 @@ def login():
         return render_template("login.html.j2")
 
 
-@views.route("/user")
+@views.route("/user", methods=["POST", "GET"])
 def user():
+    email = None
     if "user" in session:
         user = session["user"]
-        return render_template("user.html.j2", user=user)
+        if request.method == "POST":
+            email = request.form["email"]
+            session["email"] = email
+            flash("Email saved.")
+        else:
+            if "email" in session:
+                email = session["email"]
+        return render_template("user.html.j2", user=user, email=email)
     else:
         return redirect(url_for("views.login"))
 
@@ -54,5 +62,6 @@ def logout():
     if "user" in session:
         session.pop("user", None)
         session.pop("logged_in", None)
+        session.pop("email", None)
         flash("You are now logged out.", "info")
     return render_template("login.html.j2")
