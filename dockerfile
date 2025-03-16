9f7d7d3c1d29096c3d0e0dcc0da40cf4af318192
Joe Foster <josephrfoster@protonmail.com>
2025-03-15 21:00:09 -0700

    reintegration of classifier and tweaks to dockerfile for cloud run

--- dockerfile
+++ dockerfile
@@ -2,7 +2,7 @@
 FROM python:3.10-slim
 
 # By default, listen on port 5000
-EXPOSE 5000/tcp
+EXPOSE 8080
 
 # Set the working directory in the container
 WORKDIR /app
@@ -15,6 +15,9 @@ RUN pip install -r requirements.txt
 
 # Copy the content of the local src directory to the working directory
 COPY . ./
+
+# Copy model into working directory
+COPY home/joe/models/covnext_large.pth /models/dognet-convnext_large.pth
 
 # Specify the command to run on container start
 CMD [ "python", "./app.py" ]
--- website/static/index.js
+++ website/static/index.js
@@ -2,8 +2,15 @@ function changeNavbarColor() {
     const navbar = document.querySelector(".navbar");
     const hue = Math.floor(Math.random() * 360);
 
-    let backgroundColor = `hsl(${hue}, 80%, 80%)`;
-    let textColor = `hsl(${(hue + 180) % 360}, 80%, 40%)`;
+    let backgroundColor = `hsl(${hue}, 70%, 60%)`;
+    let textColor;
+
+    if (hue >= 45 && hue <= 165) {
+        textColor = "#333333";
+    }
+    else {
+        textColor = "white";
+    }
 
     navbar.style.transition = "background-color 1.5s ease-in-out";
     navbar.style.backgroundColor = backgroundColor;
@@ -13,6 +20,14 @@ function changeNavbarColor() {
         link.style.transition = "color 1.5s ease-in-out";
         link.style.color = textColor;
     });
+
+    const togglerIcon = document.querySelector(".navbar-toggler-icon");
+    if (togglerIcon) {
+        togglerIcon.style.filter = textColor === "white" ? "invert(1)" : "invert(0)";
+    }
+
+
 }
+
 
 setInterval(changeNavbarColor, 3000);
